aadabd45c0aaf6c17ecca0515d771ae5925d402d
Mark Jardine <markjardine27@gmail.com>
2023-06-29 21:24:51 +0100

    Debugging

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -1,7 +1,7 @@
 use advent_of_code::load_input_file;
 use advent_of_code::get_number;
 fn main() {
-    let file_as_string = load_input_file("inputs\\2022_day_1_input.txt").unwrap();
+    let file_as_string = load_input_file("inputs/2022_day_1_input.txt").unwrap();
     //println!("{}",file_as_string);
 
     let elf_parts = file_as_string.split("\n\n");
@@ -9,12 +9,11 @@ fn main() {
     let mut totals: Vec<u16> = Vec::new();
 
     for (index, elf) in elves.iter().enumerate() {
-        println!("elf{} -- index{}", elf, index);
+        println!("\n{} \nindex:{}\n---------", elf, index);
         let elf_cals: Vec<u16> ;
-        for s in elf {
-            let num: Option<u32> = get_number(s, '\n');
-
-        }
+        // for cal in elf {
+        //     println!("{}",cal);
+        // }
 
         // totals.push( elf_cal_parts );
         //  totals[index] += cal as u16;
