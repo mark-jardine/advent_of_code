ff4d320b225bfb3e2174807abd9d7d2a58941653
Mark Jardine <markjardine27@gmail.com>
2023-07-11 14:11:40 +0100

    removing println

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -1,14 +1,12 @@
 use advent_of_code::load_input_file;
 fn main() {
     let file_as_string = load_input_file("inputs/2022_day_1_input.txt").unwrap();
-    //println!("{}",file_as_string);
 
     let elf_parts = file_as_string.split("\n\n");
     let elves = elf_parts.collect::<Vec<&str>>();
     let mut totals: Vec<u64> = Vec::new();
 
     for (index, elf) in elves.iter().enumerate() {
-        // println!("\n{} \nindex:{}\n---------", elf, index);
         let mut elf_cals: Vec<u64> = Vec::new();
         for cal in elf.split("\n") {
             elf_cals.push(cal.parse().unwrap_or(0));
